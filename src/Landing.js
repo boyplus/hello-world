b8972c1077acbdd80cbbb3e1dc8beb84605353d7
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2019-11-05 22:54:24 +0700

    add package-lock

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 class Landing extends React.Component{
 	render(){
 		return(
@@ -8,15 +9,15 @@ class Landing extends React.Component{
 
             <div id="news" class="row"  >
               <div class="col-2 col-md-2 col-sm-2"></div>
-              <div class="col-4 col-md-4 col-sm-4"> <a href="#two"><img src="/images/11.png" alt="logo 512" width="100%" /></a></div>
-              <div class="col-4 col-md-4 col-sm-4"> <a href="#one"><img src="/images/44.png" alt="logo 512" width="100%" /></a></div>
+              <div class="col-4 col-md-4 col-sm-4"> <AnchorLink href="#two"><img src="/images/11.png" alt="logo 512" width="100%" /></AnchorLink></div>
+              <div class="col-4 col-md-4 col-sm-4"> <AnchorLink href="#one"><img src="/images/44.png" alt="logo 512" width="100%" /></AnchorLink></div>
               <div class="col-2 col-md-2 col-sm-2"></div>
             </div>
 
             <div class="row">
-              <div class="col-4 col-md-4 col-sm-4" mar>  <a href="#six1"><img src="/images/22.png" alt="logo 512" width="100%" style={{ marginTop: "50px" }} /></a></div>
+              <div class="col-4 col-md-4 col-sm-4" mar>  <AnchorLink href="#six1"><img src="/images/22.png" alt="logo 512" width="100%" style={{ marginTop: "50px" }} /></AnchorLink></div>
               <div class="col-4 col-md-4 col-sm-4"><img src="/images/Logopng.png" alt="logo 512" width="100%" /></div>
-              <div class="col-4 col-md-4 col-sm-4" >  <a href="#one"><img src="/images/555.png" alt="logo 512" width="100%" style={{ marginTop: "50px" }} /></a></div>
+              <div class="col-4 col-md-4 col-sm-4" >  <AnchorLink href="#one"><img src="/images/555.png" alt="logo 512" width="100%" style={{ marginTop: "50px" }} /></AnchorLink></div>
             </div>
 
             <div class="row">
